ff611221a2eb95ae82498fd2d96ed97dcc018f1f
Michael de Vries <michael.vries@mac.com>
2017-05-11 21:55:59 +0200

    Changed S3SignerAWS URL to own repo

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "VaporS3Signer",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/JustinM1/S3SignerAWS.git",
+        .Package(url: "https://github.com/MichaelNL/S3SignerAWS.git",
                  majorVersion: 1),
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1)
     ]
