f7394bf3af44f58981adf65dc3753cbd6140d277
Michael de Vries <michael.vries@mac.com>
2017-05-11 22:11:22 +0200

    Fixed version syntax error

--- Package.swift
+++ Package.swift
@@ -4,8 +4,7 @@ let package = Package(
     name: "VaporS3Signer",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/MichaelNL/S3SignerAWS.git",
-                 majorVersion: "1.3.1"),
+        .Package(url: "https://github.com/MichaelNL/S3SignerAWS.git", "1.3.1"),
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1)
     ]
 )
