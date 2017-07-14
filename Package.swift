9323d5516fe2c73d34f1b5079fe633a36ca5e98d
Michael de Vries <michael.vries@mac.com>
2017-07-14 21:21:52 +0200

    Changed used version of S3SignerAWS to 2.0.2

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "VaporS3Signer",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/MichaelNL/S3SignerAWS.git", "2.0.1"),
+        .Package(url: "https://github.com/MichaelNL/S3SignerAWS.git", "2.0.2"),
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2)
     ]
 )
